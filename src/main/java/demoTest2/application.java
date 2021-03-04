8b3efada240ce85c1005b7c7d3a3711d4b856f0d
Valentin-alix <73835897+Valentin-alix@users.noreply.github.com>
2021-03-04 12:36:01 +0100

    Initial commit

---
+++ src/main/java/demoTest2/application.java
@@ -0,0 +1,23 @@
+package demoTest2;
+
+public class application {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+	public static int add(int a,int b)
+	{
+		return a + b;
+	}
+	
+	public int sub(int a, int b)
+	{
+		return 0;
+	}
+}
